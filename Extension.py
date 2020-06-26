647b44d43c1fd2010506046aef00b3ae06f824dc
SupremeSoul36 <66683889+SupremeSoul36@users.noreply.github.com>
2020-06-26 22:28:01 +0530

    Created Extension.py
    Finds the extension of file

---
+++ Extension.py
@@ -0,0 +1,11 @@
+
+fh=input("Input the Filename:")
+x=fh.split('.')
+if(x[1]=='py'):
+    print('The extension of the file is python')
+elif(x[1]=='java'):
+    print('The extension of the file is java')
+elif(x[1]=='c'):
+    print('The extension of the file is C')
+elif(x[1]=='cpp'):
+    print('The extension of the file is C++')
